648786a8b8ffcef7759c03ab6a840991c857a611
Robert <anutarobertmarian@gmail.com>
2023-09-13 06:05:17 +0100

    Final Commit

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,42 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const path = require("path");
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const path = require('path')
 
-const saucesRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces')
+const userRoutes = require('./routes/user')
 
-const app = express();
+const app = express()
 try {
-    mongoose.connect("mongodb+srv://anutar:R%40bert123@cluster0.ptmajah.mongodb.net/")
-        .then(() => {
-            console.log("Successfully connected to MongoDB Atlas!");
-        });
+  mongoose
+    .connect('mongodb+srv://anutar:R%40bert123@cluster0.ptmajah.mongodb.net/')
+    .then(() => {
+      console.log('Successfully connected to MongoDB Atlas!')
+    })
 } catch (error) {
-    console.log("Unable to connect to MongoDB Atlas!");
-    console.error(error);
+  console.log('Unable to connect to MongoDB Atlas!')
+  console.error(error)
 }
 
-app.use(express.json());
+app.use(express.json())
 
 // all types of access
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
 
-// app.use(bodyParser.json());
+// setting up a middleware
+app.use('/api/sauces', saucesRoutes)
+app.use('/api/auth', userRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
-// Serve static files
-app.use('/api/sauces', saucesRoutes);
-app.use('/api/auth', userRoutes);
-app.use("/images", express.static(path.join(__dirname, "images")));
-
-module.exports = app;
+module.exports = app
